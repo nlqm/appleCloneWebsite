dd96e98995297e6317f93e8df14ea0d87ab38d48
nlqm <maiqnguyenluu41101@gmail.com>
2024-12-16 22:10:31 +1100

    first commit from Mia

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Navbar from "./components/navBar";
+import Hero from "./components/hero";
+import Highlights from "./components/Highlights";
+import Model from "./components/Model";
+import Features from "./components/Features";
+import HowItWorks from "./components/HowItWorks";
+import Footer from "./components/Footer";
+
+const App = () => {
+  return (
+    <main className="bg-black">
+      <Navbar />
+      <Hero />
+      <Highlights />
+      <Model />
+      <Features />
+      <HowItWorks />
+      <Footer />
+    </main>
+  )
+}
+
+export default App
